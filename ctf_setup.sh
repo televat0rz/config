dbe41f0bbc0d529e9848819095d561d73e2cc5af
televat0rz <149993290+televat0rz@users.noreply.github.com>
2024-06-15 17:02:27 -0500

    Update ctf_setup.sh

--- ctf_setup.sh
+++ ctf_setup.sh
@@ -32,7 +32,7 @@ tmux send-keys -t 0:config.3 "vi /etc/hosts"
 # Create and setup 'recon' window panes
 tmux new-window -n recon
 sleep 1
-tmux split-window -h -t 0:recon.0 && tmux resize-pane -R 42
+tmux split-window -h -t 0:recon.0 && tmux resize-pane -R 52
 sleep 2
 tmux send-keys -t 0:recon.0 "autorecon --only-scans-dir --no-port-dirs --dirbuster.threads 50 --dirbuster.wordlist /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-small.txt --subdomain-enum.threads 70 --subdomain-enum.wordlist /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt --vhost-enum.threads 70 --vhost-enum.wordlist /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt -vvv $TARGET" C-m
 sleep 1
@@ -41,12 +41,12 @@ tmux send-keys -t 0:recon.1 "cd results/*/scans;tree -h"
 # Create another unnamed window and split the pane horizontally
 tmux new-window
 sleep 1
-tmux split-window -h -t 0:2.0 && tmux resize-pane -R 26
+tmux split-window -h -t 0:2.0 && tmux resize-pane -R 32
 tmux split-window -v -t 0:2.0 && tmux resize-pane -D 32
-tmux split-window -v -t 0:2.1 && tmux resize-pane -U 26
+tmux split-window -v -t 0:2.1 && tmux resize-pane -U 28
 sleep 2
 tmux send-keys -t 0:2.1 'pwncat-cs -p 1337' C-m
-tmux send-keys -t 0:2.2 'http-server 80' C-m
+tmux send-keys -t 0:2.2 'http-put-server 80' C-m
 
 # Select the final pane for the cursor in 'recon'
 tmux select-window -t 0:recon
